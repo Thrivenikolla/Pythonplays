576aab1f424a1f6de4680eec3140ffb0f8ede969
Sri <thriveni.kolla>
2025-05-15 20:57:42 +0530

    added code

---
+++ args.py
@@ -0,0 +1,8 @@
+def sum_num(*args): #*args allows a function to accept any num of positional args
+    total = 0
+    for num in args:
+        total += num
+    return total
+    
+print(sum_num(1, 9, 33))
+print(sum_num(1, 9, 8, 2, 5))
