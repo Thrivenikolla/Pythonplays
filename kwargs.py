15583337c04bd652447c1042407f35a30f45a77a
Sri <thriveni.kolla>
2025-05-15 21:37:51 +0530

    added code

---
+++ kwargs.py
@@ -0,0 +1,5 @@
+def display(**kwargs):  #*args allows a function to accept any num of keyword args
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+display(name="sri", city="hyd")
