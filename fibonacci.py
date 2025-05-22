dd440014eb66671e5a3f0391780975c274afa903
Sri <thriveni.kolla>
2025-05-22 21:09:33 +0530

    added fibonacci code

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+
+print(fibonacci(int(input("Enter a number to find its fibonacci series : "))))
